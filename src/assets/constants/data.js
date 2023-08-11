675968775ac1ec23fa3739ae64361a28cf63b97f
Ankit Meena <ankitmeena9783226195@gmail.com>
2023-08-11 11:50:22 +0530

    K

--- src/assets/constants/data.js
+++ src/assets/constants/data.js
@@ -1,5 +1,1 @@
-// export const url = "https://macbeas-loadbalancer-1530178383.ap-northeast-1.elb.amazonaws.com/";
-// export const url = "http://3.114.19.104/";
 export const url = "https://api.macbease.com/";
-// export const url = "http://localhost:5050/";
-//testing 1.
